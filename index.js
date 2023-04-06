8fbb81c368b764f37d99c9bb332f149a3895438a
Janhavi Bajpai <95091521+janhavibajpai01@users.noreply.github.com>
2023-04-06 22:55:00 +0530

    dice game

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImagesSource = "images/" + randomDiceImage;
+
+document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImagesSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+
+var randomImagesSource = "images/" + randomDiceImage;
+
+document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImagesSource);
